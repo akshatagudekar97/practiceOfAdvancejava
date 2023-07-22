ea3cf429ee4fc1c84397a26a20e624464bf156f5
akshatagudekar97 <akshatagudekar97@gmail.com>
2023-07-22 11:34:48 +0530

    first crude start WebserviceApplication

---
+++ webservice/src/main/java/com/main/webservice/app/WebserviceApplication.java
@@ -0,0 +1,18 @@
+package com.main.webservice.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.main.webservice.app.model.student;
+
+@SpringBootApplication
+public class WebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceApplication.class, args);
+		
+		
+		
+	}
+
+}
---
+++ webservice/src/main/java/com/main/webservice/app/model/student.java
@@ -0,0 +1,11 @@
+package com.main.webservice.app.model;
+
+import lombok.Data;
+
+@Data
+public class student {
+
+	private int id;
+	private String name;
+	
+}
