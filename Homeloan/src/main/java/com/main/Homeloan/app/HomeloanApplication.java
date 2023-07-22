1e5085bbbbec268cb169bbe070e287456586cdb9
akshatagudekar97 <akshatagudekar97@gmail.com>
2023-07-22 23:03:47 +0530

    new work start

---
+++ Homeloan/src/main/java/com/main/Homeloan/app/HomeloanApplication.java
@@ -0,0 +1,16 @@
+package com.main.Homeloan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.main.Homeloan.app.model.student;
+
+@SpringBootApplication
+public class HomeloanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeloanApplication.class, args);
+		
+	}
+
+}
---
+++ Homeloan/src/main/java/com/main/Homeloan/app/model/student.java
@@ -0,0 +1,12 @@
+package com.main.Homeloan.app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class student {
+	int id;
+	String name;
+}
